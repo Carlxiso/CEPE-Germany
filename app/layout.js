a90ffae849ee111e5bb1e83de52fc2f9be72a1bf
Carl-iso <lavaredasnarciso@gmail.com>
2026-02-12 11:02:47 +0100

    CEPE Germany Website Project  - Defining Routes and Pages

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "CEPE - Alemanha",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
 export default function Page() {
-  return (
-    <html lang="en">
-      <body>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </body>
-    </html>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
