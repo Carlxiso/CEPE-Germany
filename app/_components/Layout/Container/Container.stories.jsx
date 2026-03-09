6784525c507584bb99fc9b987b785f707d707471
Carl-iso <lavaredasnarciso@gmail.com>
2026-03-09 17:38:59 +0100

    CEPE Alemanha Website Project - Container Component

---
+++ app/_components/Layout/Container/Container.stories.jsx
@@ -0,0 +1,10 @@
+import Container from "./Container";
+
+const meta = {
+  title: "Layout/Container",
+  component: Container,
+};
+
+export default meta;
+
+export const Default = () => <Container />;
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,10 +1,14 @@
+import Container from "../_components/Layout/Container/Container";
+
 export const metadata = {
   title: "Blog",
 };
 export default function Page() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Blog Page</h1>
+      <Container>
+        <h1 className="text-3xl font-bold underline">Blog Page</h1>
+      </Container>
     </div>
   );
 }
