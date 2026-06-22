b55cb5911d46a477ae328227865fcd4129254976
Carl-iso <lavaredasnarciso@gmail.com>
2026-06-22 14:28:27 +0200

    CEPE Alemanha Website Project - Feature: Blog - Handling Thumbnail Function - Creating and Editing Article

--- app/features/blog/components/CreateNewArticle/CreateNewArticle.js
+++ app/features/blog/components/CreateNewArticle/CreateNewArticle.js
@@ -5,6 +5,8 @@ import styles from "./CreateNewArticle.module.css";
 import { useEffect, useState } from "react";
 import { supabase } from "@/app/_lib/auth/supabase";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 // type BlogPost = {
 //   title: string
 
@@ -23,6 +25,7 @@ import { useSearchParams } from "next/navigation";
 
 export default function CreateNewArticle() {
   const [category, setCategory] = useState("");
+  const [loading, setLoading] = useState(false);
   const [categories, setCategories] = useState([]);
   const [loadingArticle, setLoadingArticle] = useState(false);
   const [title, setTitle] = useState("");
@@ -31,6 +34,8 @@ export default function CreateNewArticle() {
   const [user, setUser] = useState(null);
   const searchParams = useSearchParams();
   const articleId = searchParams.get("id");
+
+  const router = useRouter();
 
   useEffect(() => {
     supabase.auth.getUser().then(({ data }) => setUser(data.user));
@@ -96,21 +101,109 @@ export default function CreateNewArticle() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const wordCount =
+      content
+        ?.replace(/<[^>]*>/g, "")
+        ?.trim()
+        ?.split(/\s+/)?.length || 0;
+
+    const readTime = Math.ceil(wordCount / 200);
+
+    let thumbnailUrl = "";
+
+    if (thumbnail && thumbnail.name) {
+      const fileExtension = thumbnail.name.split(".").pop();
+      const fileName = `${Date.now()}.${fileExtension}`;
+      const filePath = `${user?.id}/${fileName}`;
+
+      const { error: uploadError } = await supabase.storage
+        .from("cepe-blog-bucket")
+        .upload(filePath, thumbnail);
+
+      if (uploadError) {
+        console.log("Thumbnail Upload Error", uploadError);
+        setLoading(false);
+        return;
+      }
+
+      const { data: publicUrlData } = supabase.storage
+        .from("cepe-blog-bucket")
+        .getPublicUrl(filePath);
+      thumbnailUrl = publicUrlData.publicUrl;
+    }
+
+    const slug = title
+      ?.toLowerCase()
+      ?.trim()
+      ?.replace(/[^\w\s-]/g, "") // Remove special caracthers
+      .replace(/\s+/g, "-") // Remove spaces with dashes
+      .replace(/-+/g, "-"); // Clean up multiple dashes
+
+    // Edit Mode
+    if (articleId) {
+      const { error } = await supabase
+        .from("articles")
+        .update({
+          title,
+          content,
+          category_id: category,
+          thumbnail: thumbnailUrl || undefined,
+          read_time: readTime,
+        })
+        .eq("id", articleId)
+        .eq("profile_id", user?.id);
+
+      if (error) {
+        console.log("Update Error:", error);
+        setLoading(false);
+        return;
+      }
+      // Success MSG
+    } else {
+      const { data, error } = await supabase
+        .from("articles")
+        .insert({
+          title,
+          content,
+          category_id: category,
+          thumbnail: thumbnailUrl || undefined,
+          read_time: readTime,
+          slug,
+          profile_id: user?.id,
+        })
+        .select("id")
+        .single();
+
+      if (error) {
+        console.log("Failed to Create an Article:", error);
+        setLoading(false);
+        return;
+      }
+      // Success MSG - Article Created Successfully
+      router.push(`/admin/blog/criar?id=${data.id}`);
+    }
+    setLoading(false);
+  };
+
   return (
     <section className={styles.createPostSection}>
       <div className={styles.createPostCard}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           {/* Thumbnail */}
           <div className={styles.thumbnailSection}>
             <Image
               width={500}
               height={500}
               src={
-                typeof thumbnail === "String"
-                  ? thumbnail
-                  : thumbnail
-                    ? URL.createObjectURL(thumbnail)
-                    : "/default/defaultArticle.png"
+                thumbnail
+                  ? typeof thumbnail === "string"
+                    ? thumbnail
+                    : URL.createObjectURL(thumbnail)
+                  : "/default/defaultArticle.png"
               }
               className={styles.thumbnailPreview}
               alt="Thumbnail Preview"
@@ -137,8 +230,12 @@ export default function CreateNewArticle() {
             <input
               id="title"
               type="text"
-              placeholder="Enter a catchy title"
+              placeholder="Escreva o titulo do artigo"
               className={styles.input}
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
             />
           </div>
 
@@ -147,8 +244,12 @@ export default function CreateNewArticle() {
             <label htmlFor="content">Conteúdo</label>
             <textarea
               id="content"
-              placeholder="Write content..."
+              placeholder="Escreva o conteúdo do artigo..."
               className={styles.textarea}
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
               required
             />
           </div>
@@ -176,6 +277,17 @@ export default function CreateNewArticle() {
           <div className={styles.submitWrapper}>
             <button type="submit" className={styles.submitButton}>
               Criar Artigo
+              {loading ? (
+                articleId ? (
+                  <>Atualizar Artigo</>
+                ) : (
+                  <>Criar Artigo</>
+                )
+              ) : articleId ? (
+                <>Atualizar Artigo</>
+              ) : (
+                <>Criar Artigo</>
+              )}
             </button>
           </div>
         </form>
