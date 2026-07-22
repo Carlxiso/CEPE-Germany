f2f1923afcf40edb7b4b8d8ad0b6052c09aad168
Carl-iso <lavaredasnarciso@gmail.com>
2026-07-22 16:41:10 +0100

    CEPE Alemanha Website Project - Feature: Diagnostic Test - Loading Questions from a Fake API - Fetching Data

--- app/_components/Layout/DiagnosticTest/DiagnosticTest.jsx
+++ app/_components/Layout/DiagnosticTest/DiagnosticTest.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useReducer } from "react";
 import CTASection from "../../UI/CTASection/CTASection";
 import HeaderDiagnosticTest from "./HeaderDiagnosticTest/HeaderDiagnosticTest";
 import InstructionsDiagnosticTest from "./InstructionsDiagnosticTest/InstructionsDiagnosticTest";
@@ -9,7 +11,38 @@ const cta = {
   text: "A língua portuguesa une culturas, histórias e continentes. Aceite o desafio e descubra até que ponto o seu conhecimento abarca a língua portuguesa. Faça o teste diagnóstico e explore a diversidade, a riqueza e a vitalidade de uma língua verdadeiramente global.",
 };
 
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unkonwn");
+  }
+}
 export default function DiagnosticTest() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:3001/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <CTASection headline={cta.headline} text={cta.text}>
       <HeaderDiagnosticTest />
